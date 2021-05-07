4bdfe441929a1d3aa1c1b9e1a2639a4efbc3c25d
ElroiGeek <83841401+ElroiGeek@users.noreply.github.com>
2021-05-07 16:30:47 -0400

    Add files via upload

---
+++ Mad Liba.py
@@ -0,0 +1,6 @@
+noun1 = input("Enter noun: ")
+adjective = input("Enter adjective: ")
+noun2 = input("Enter noun: ")
+
+print(f"After hiding the painting in his {noun1} for two years, he grew {adjective} and tried to sell it to a/an {noun2} in Flroence, but was caught.")
+
